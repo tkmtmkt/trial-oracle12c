6671afe86533bbb1b40f1705b14e51d3d5cde9cc
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-01-07 23:48:39 +0900

    fix: PDB作成設定削除

--- share/dbca_createdb.sh
+++ share/dbca_createdb.sh
@@ -2,5 +2,3 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
 dbca -silent -createDatabase -responseFile $SCRIPT_DIR/dbca_testdb.rsp
-dbca -silent -createPluggableDatabase -sourceDB testdb -pdbName pdb1 -pdbAdminPassword oracle
-dbca -silent -createPluggableDatabase -sourceDB testdb -pdbName pdb2 -pdbAdminPassword oracle
--- share/dbca_deletedb.sh
+++ share/dbca_deletedb.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
-dbca -silent -deletePluggableDatabase -sourceDB testdb -pdbName pdb1
-dbca -silent -deletePluggableDatabase -sourceDB testdb -pdbName pdb2
 dbca -silent -deleteDatabase -responseFile $SCRIPT_DIR/dbca_testdb.rsp
