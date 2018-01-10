bf93c519aab69e5956b8917546abc3a31845e50f
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-01-10 20:33:22 +0900

    fix: デフォルトデータファイル位置指定追加

--- share/dbca_createdb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$(cd $(dirname $0);pwd)
-
-dbca -silent -createDatabase -responseFile $SCRIPT_DIR/dbca_testdb.rsp
--- share/dbca_deletedb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$(cd $(dirname $0);pwd)
-
-dbca -silent -deleteDatabase -responseFile $SCRIPT_DIR/dbca_testdb.rsp
