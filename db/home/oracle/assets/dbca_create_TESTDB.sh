0de03e8c8bd3e37bfe05fc62973634530d956d17
Takamatsu Makoto <tkmtmkt@gmail.com>
2020-04-23 19:26:01 +0900

    modify: Change script for create database

--- db/home/oracle/assets/dbca_create_TESTDB.sh
+++ db/home/oracle/assets/dbca_create_TESTDB.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
-dbca -silent -responseFile $SCRIPT_DIR/dbca_create_TESTDB.rsp
+DBNAME=TESTDB
+DBPASS=P@ssw0rd
+BASE_DIR=/opt/oracle
+DATA_DIR=${BASE_DIR}/oradata
+FRA_DIR=${BASE_DIR}/fast_recovery_area
+
+dbca -silent \
+  -createDatabase \
+  -templateName General_Purpose.dbc \
+  -gdbName ${DBNAME} \
+  -sid ${DBNAME} \
+  -sysPassword ${DBPASS} \
+  -systemPassword ${DBPASS} \
+  -emConfiguration DBEXPRESS \
+  -datafileDestination ${DATA_DIR} \
+  -redoLogFileSize 100 \
+  -recoveryAreaDestination ${FRA_DIR} \
+  -characterSet AL32UTF8 \
+  -nationalCharacterSet AL16UTF16 \
+  -databaseType MULTIPURPOSE \
+  -totalMemory 4800 \
+  -initParams db_create_file_dest="${DATA_DIR}",db_recovery_file_dest="${FRA_DIR}",db_recovery_file_dest_size=30720
